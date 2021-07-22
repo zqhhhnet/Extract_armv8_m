0116c0e9b0f6597cbaf4bea0ff9f14257a4137ed
zqhhhnet <561551200@qq.com>
2021-07-22 11:22:32 +0800

    update

--- k-armv8.1-m/test.s
+++ k-armv8.1-m/test.s
@@ -1,4 +1,4 @@
-main:
-  // vmax.S8 q2, q1, q0	// S16 +, s8 -, U16 +, U8 +, S32 +, U32 +
+start:
+  // vmax.U32 q2, q1, q0	// S16 +, s8 -, U16 +, U8 +, S32 +, U32 +
   vmaxnm.F16 q2, q1, q0
-  exit._0
+end
